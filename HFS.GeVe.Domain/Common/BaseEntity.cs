16433b3fd65ac1def9afb850a5879ae2d96cddc2
ubistart-henrique <henrique.santos@ubistart.com>
2022-09-05 20:00:40 -0300

    Adicionar arquivos de projeto.

---
+++ HFS.GeVe.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿
+namespace HFS.GeVe.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        protected Guid Id { get; init; }
+        public bool Ativo { get; init; }
+    }
+}
---
+++ HFS.GeVe.Domain/Entities/Vaga.cs
@@ -0,0 +1,10 @@
+﻿using HFS.GeVe.Domain.Common;
+
+namespace HFS.GeVe.Domain.Entities;
+
+public class Vaga : BaseEntity
+{
+    public string Codigo { get; init; } = string.Empty;
+    public Guid AndarId { get; init; }
+    public decimal? ValorHora { get; init; }
+}
